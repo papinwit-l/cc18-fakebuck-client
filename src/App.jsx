05b07dcd1016ebe33a924af6396814a8ebfb3d21
iPaE <pae.papinwit@gmail.com>
2024-10-02 13:50:58 +0700

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
         Header
       </header>
       <main className="relative flex bg-blue-200 border h-screen pt-14">
-        <div className="fixed top-14 h-full bg-red-200 w-[280px] overflow-auto">
+        <div className="fixed top-14 h-[calc(100%-3.5rem)] bg-red-200 w-[280px] overflow-auto">
           <div className="btn btn-xl m-10">Menu Item</div>
           <div className="btn btn-xl m-10">Menu Item</div>
           <div className="btn btn-xl m-10">Menu Item</div>
@@ -17,8 +17,10 @@ function App() {
           <div className="btn btn-xl m-10">Menu Item</div>
           <div className="btn btn-xl m-10">Menu Item</div>
         </div>
-        <div className="fixed h-full right-[280px] left-[280px]">Content</div>
-        <div className="fixed top-14 right-0 h-full bg-red-200 w-[280px] overflow-auto">
+        <div className="fixed top-14 h-[calc(100%-3.5rem)] right-[280px] left-[280px]">
+          Content
+        </div>
+        <div className="fixed top-14 right-0 h-[calc(100%-3.5rem)] bg-red-200 w-[280px] overflow-auto">
           <div className="btn btn-xl m-10">Menu Item</div>
           <div className="btn btn-xl m-10">Menu Item</div>
           <div className="btn btn-xl m-10">Menu Item</div>
