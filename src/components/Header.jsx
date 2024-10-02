bab32b7ee4ba8e56eb446fdb479b15eadc2ad32a
iPaE <pae.papinwit@gmail.com>
2024-10-02 14:13:53 +0700

    layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="bg-orange-200 h-14 w-full fixed top-0 z-10">
       <div className="flex gap-2 items-center">
-        <FacebookLogo />
+        <FacebookLogo className="w-12" />
       </div>
     </header>
   );
