ccfe9dacc21a68241bfcbce26aed313be55453ee
iPaE <pae.papinwit@gmail.com>
2024-10-02 13:45:29 +0700

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 function App() {
   return (
-    <>
-      <p>Fakebuck</p>
-      <button className="btn">Button</button>
-    </>
+    <div className="min-h-screen bg-yellow-100">
+      <header className="bg-orange-200 h-14 w-full fixed top-0 z-10">
+        Header
+      </header>
+      <main className="relative flex bg-blue-200 border h-screen pt-14">
+        <div className="fixed top-14 h-full bg-red-200 w-[280px] overflow-auto">
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+        </div>
+        <div className="fixed h-full right-[280px] left-[280px]">Content</div>
+        <div className="fixed top-14 right-0 h-full bg-red-200 w-[280px] overflow-auto">
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+        </div>
+      </main>
+    </div>
   );
 }
 
